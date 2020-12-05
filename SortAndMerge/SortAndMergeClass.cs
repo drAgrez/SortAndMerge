e7bfc5705191e0c0c356ec7bafbf6ace8dec081c
drAgrez <65555517+drAgrez@users.noreply.github.com>
2020-12-05 20:53:36 +0300

    Update SortAndMergeClass.cs

--- SortAndMerge/SortAndMergeClass.cs
+++ SortAndMerge/SortAndMergeClass.cs
@@ -35,7 +35,6 @@ namespace SortAndMerge
         {
             foreach (string path in Directory.GetFiles(Environment.CurrentDirectory, "split*.dat"))
             {
-                // я понимаю, что в этом месте можно немного оптимизировать, используя не лист, но я сделала по-простому
                 List<long> content = new List<long>();
                 // Чтение строк из файла
                 using (StreamReader sr = new StreamReader(path)) 
